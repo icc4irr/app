a0ab738577d51e9a18eb908bda91fde0c4b773be
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2025-08-18 03:46:47 +0300

    add tab panel compute Q khat

---
+++ sources/tabpanel_compQkhat.R
@@ -0,0 +1,41 @@
+
+# tabpanel - Compute Q/khat -------------------------------------------------
+
+tabpanel_compQkhat <- tabPanel(
+  "Compute Design Factors",
+  wellPanel(
+    h4("Compute Q khat given your data:"),
+  ),
+  ## uplaod file and output panels
+  sidebarLayout(
+    sidebarPanel(
+      h2("Step 1"),
+      fileInput("file2", "Upload your data", accept = c(".rds", ".Rdat", ".csv", ".txt", ".sav", ".xlsx"))
+      
+    ),
+    mainPanel(
+      tabsetPanel(
+        div(style = "height: 180px; overflow-y: auto;",
+            DT::dataTableOutput("table2"))
+      )
+    )
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      h2("Step 2"),
+      fluidRow(
+        column(6, uiOutput("subject_comp")),
+        column(6, uiOutput("rater_comp"))
+      ),
+      uiOutput("computebutton")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Results",
+                 uiOutput("compQkhat_card")
+        ),
+        tabPanel("Table", DT::dataTableOutput(("Qktab_comp"))),
+      )
+    )
+  )
+)
